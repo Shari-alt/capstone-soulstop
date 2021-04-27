96c97980c1b1a17674768d95ea28327d534be940
Shari Nielsen <sharinielsen@web.de>
2021-04-27 11:12:24 +0200

    Added the background picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
-export default function Header () { 
-    return (
-        
-    )
+export default function Header() {
+  return (
+    <div>
+      <p> hello, Alex said this is the header</p>
+    </div>
+  );
 }
